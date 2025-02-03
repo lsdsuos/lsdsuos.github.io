558a2011cb5d5a0ffee6417b8da072873d2a34a0
dbkim <lsds.uos@gmail.com>
2025-02-03 17:21:14 +0900

    demo

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     notice: "./src/notice.js",
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "[name].js",
   },
   module: {
@@ -119,7 +119,7 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "docs"),
     }, // 정적 파일 제공
     compress: true, // gzip 압축 사용 여부
     port: 3000, // 개발 서버 포트
